04f16e2f982208acdb5aa602432f3ebd59c84130
aditi178 <112297220+aditi178@users.noreply.github.com>
2023-02-18 14:53:41 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( correct) {
+  setScreen("screen3");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button4", "click", function( correct) {
+  setScreen("screen4");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen6");
+});
